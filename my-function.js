cf5acf171804e7d4a4988aea3f6b8c8db8f99b94
AjayS2023 <99766251+AjayS2023@users.noreply.github.com>
2023-10-08 17:41:01 -0400

    Made some spacing changes

--- my-function.js
+++ my-function.js
@@ -1,27 +1,27 @@
 exports.handler = async (event) => {
   
-  const keyword = event.queryStringParameters && event.queryStringParameters.keyword;
+	const keyword = event.queryStringParameters && event.queryStringParameters.keyword;
   
-  // if there is no keyword, we respond saying there is no keyword
-  if(!keyword){
+	// if there is no keyword, we respond saying there is no keyword
+	if(!keyword){
 		const response = {
-		  statusCode: 400,
-		  body: 'There is no keyword. Please put in a keyword.',
-		  headers: {
-		    'Content-Type': 'text/plain',
-		  },
+			statusCode: 400,
+			body: 'There is no keyword. Please put in a keyword.',
+			headers: {
+				'Content-Type': 'text/plain',
+			},
 		};
 		return response;
-  }
+	}
   
-  // if there is a keyword, it sends a message of me saying the keyword 
-  const response = {
-    statusCode: 200,
-    body: `Ajay says ${keyword}.`,
-    headers: {
-      'Content-Type': 'text/plain',
-    },
-  };
-  
-  return response;
+	// if there is a keyword, it sends a message of me saying the keyword 
+	const response = {
+		statusCode: 200,
+		body: `Ajay says ${keyword}.`,
+		headers: {
+			'Content-Type': 'text/plain',
+		},
+	};
+		
+	return response;
 };
